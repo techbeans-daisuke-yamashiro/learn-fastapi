a3aa622dbe1f338021832aa47cf8a73dddc0b3c7
techbeans-daisuke-yamashiro <daisuke.yamashiro@techbeans.co.jp>
2025-03-21 18:24:20 +0900

    論理削除フィルタを調整

--- src/models/base.py
+++ src/models/base.py
@@ -24,29 +24,3 @@ class ModelBase(TimestampMixin):
         return {c.name: getattr(self,c.name) for c in self.__table__.columns}
 
 
-# 論理削除フィルターを定義
-@sa.event.listens_for(Session, "do_orm_execute")
-def _add_filtering_deleted_at(execute_state):
-    """
-    論理削除用のfilterを自動的に適用する
-    以下のようにすると、論理削除済のデータも含めて取得可能
-    query(...).filter(...).execution_options(include_deleted=True)
-    """
-    if (
-        execute_state.is_select
-        and not execute_state.is_column_load
-        and not execute_state.is_relationship_load
-        and not execute_state.execution_options.get("include_deleted", False)
-    ):
-        for sc in SQLModel.__subclasses__():
-            print(sc.__dir__())
-            if hasattr(sc, "__table__") and hasattr(sc, "deleted_at"):
-                execute_state.statement = execute_state.statement.options(
-                    sa.orm.with_loader_criteria(
-                        sc,
-                        lambda cls: cls.deleted_at.is_(None),
-                        include_aliases=True,
-                    )
-                )
-    else:
-        return
